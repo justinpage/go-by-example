966e7df1b2c46bf40901b3800b83e2e95278bd7b
Justin Page <jpage@enthusiastnetwork.com>
2018-08-22 18:46:05 -0700

    init

---
+++ slices.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	s := make([]string, 3)
+	fmt.Println("emp:", s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len:", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	l := s[2:5]
+	fmt.Println("sl1", l) // c, d, e
+
+	l = s[0:5]
+	fmt.Println("sl2", l) // a, b, c, d, e
+
+	l = s[2:]
+	fmt.Println("sl3", l) // c, d, e, f
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < len(twoD); i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d ", twoD)
+}
---
+++ variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	b, c := 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+}
