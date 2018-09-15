4ef35e329014af5c7c9ec1f89a28b893d9a8b89b
Justin Page <justin.page@motortrend.com>
2018-09-15 13:08:10 -0700

    Mutexes, state, atomic counters
    Also includes sorting and rate limiting

---
+++ mutexes.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	state := make(map[int]int)
+	mutex := &sync.Mutex{}
+
+	var readOps uint64
+	var writeOps uint64
+
+	// Reads
+	for r := 0; r < 100; r++ {
+		go func() {
+			total := 0
+			for {
+				key := rand.Intn(5)
+				mutex.Lock()
+				total += state[key]
+				mutex.Unlock()
+				atomic.AddUint64(&readOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	// Writes
+	for w := 0; w < 10; w++ {
+		go func() {
+			for {
+				key := rand.Intn(5)
+				val := rand.Intn(100)
+				mutex.Lock()
+				state[key] = val
+				mutex.Unlock()
+				atomic.AddUint64(&writeOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	readsOpsFinal := atomic.LoadUint64(&readOps)
+	fmt.Println("readOps:", readsOpsFinal)
+	writeOpsFinal := atomic.LoadUint64(&writeOps)
+	fmt.Println("writeOps:", writeOpsFinal)
+
+	mutex.Lock()
+	fmt.Println("state:", state)
+	mutex.Unlock()
+}
