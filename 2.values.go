49b6ff24d6d5a347f876f4a358c624cbcc5bca83
Justin Page <justin.page@motortrend.com>
2019-06-18 21:58:54 -0700

    Update order to incremental files

--- 2.values.go
+++ 2.values.go
@@ -7,10 +7,11 @@ import (
 func main() {
 	fmt.Println("go" + "lang")
 
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("7.0/3.0", 7.0/3.0)
 
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
 }
+
