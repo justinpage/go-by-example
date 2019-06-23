56804df59b2cda8e64974bb0d6db273af3225961
Justin Page <justin.page@motortrend.com>
2019-06-23 13:59:57 -0700

    Rename: For

--- 5.for.go
+++ 5.for.go
@@ -3,22 +3,25 @@ package main
 import "fmt"
 
 func main() {
-
+	// Basic
 	i := 1
-	for i < 3 {
+	for i <= 3 {
 		fmt.Println(i)
 		i = i + 1
 	}
 
+	// Classic
 	for j := 7; j <= 9; j++ {
 		fmt.Println(j)
 	}
 
+	// Break
 	for {
 		fmt.Println("loop")
 		break
 	}
 
+	// Continue 
 	for n := 0; n <= 5; n++ {
 		if n%2 == 0 {
 			continue
