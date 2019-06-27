b51699c3c39f9038e718aa66de8dcfe9ad726f49
Justin Page <justin.page@motortrend.com>
2019-06-26 23:00:48 -0700

    Update switch example

--- 7.switch.go
+++ 7.switch.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
-
 	i := 2
-	fmt.Print("Write ", i, " as ")
+
+	fmt.Print("Write, ", i, " as ")
+
 	switch i {
 	case 1:
 		fmt.Println("one")
@@ -25,23 +26,14 @@ func main() {
 		fmt.Println("It's a weekday")
 	}
 
-	t := time.Now()
-
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("It's before noon")
-	default:
-		fmt.Println("It's after noon")
-	}
-
 	whatAmI := func(i interface{}) {
 		switch t := i.(type) {
 		case bool:
 			fmt.Println("I'm a bool")
 		case int:
-			fmt.Println("I'm a int")
+			fmt.Println("I'm an int")
 		default:
-			fmt.Printf("Don't know type %T\n", t)
+			fmt.Printf("Don't know type: %T", t)
 		}
 	}
 
