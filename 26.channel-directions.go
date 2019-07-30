d41b37d38be77f7320073a2e6923543058674ca8
Justin Page <justin.page@motortrend.com>
2019-07-30 00:17:15 -0700

    Remove public functions; use brief comments

--- 26.channel-directions.go
+++ 26.channel-directions.go
@@ -2,15 +2,13 @@ package main
 
 import "fmt"
 
-// This ping function only accepts a channel for sending values. It would be a
-// compile-time error to try to receive on this channel.
-func Ping(pings chan<- string, msg string) {
+// Accepts a channel for sending values, not receiving them
+func ping(pings chan<- string, msg string) {
 	pings <- msg
 }
 
-// The pong function accepts one channel for receives (pings) and a second for
-// sends (pongs).
-func Pong(pings <-chan string, pongs chan<- string) {
+// Accepts a channel for receiving values, and another for sending values
+func pong(pings <-chan string, pongs chan<- string) {
 	msg := <-pings
 	pongs <- msg
 }
@@ -18,7 +16,9 @@ func Pong(pings <-chan string, pongs chan<- string) {
 func main() {
 	pings := make(chan string, 1)
 	pongs := make(chan string, 1)
-	Ping(pings, "passed message")
-	Pong(pings, pongs)
+
+	go ping(pings, "passed message")
+	go pong(pings, pongs)
+
 	fmt.Println(<-pongs)
 }
