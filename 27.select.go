1d5fb2e0ee10dc7f426cc5c4257664d67efe5f04
Justin Page <justin.page@motortrend.com>
2019-07-31 18:18:41 -0700

    Update select example
    Included time.After case:
    https://golang.org/pkg/time/#After

--- 27.select.go
+++ 27.select.go
@@ -19,12 +19,14 @@ func main() {
 		c1 <- "two"
 	}()
 
-	for i := 0; i < 2; i++ {
+	for {
 		select {
 		case msg1 := <-c1:
 			fmt.Println("received", msg1)
 		case msg2 := <-c2:
 			fmt.Println("received", msg2)
+		case <-time.After(3 * time.Second):
+			return
 		}
 	}
 }
