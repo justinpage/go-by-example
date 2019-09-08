fe72ad258784b7a300cedd62cf66abc3b8a8a287
Justin Page <justin.page@motortrend.com>
2019-09-07 17:35:15 -0700

    Updated sorting example

--- 40.sorting.go
+++ 40.sorting.go
@@ -12,8 +12,8 @@ func main() {
 
 	ints := []int{7, 2, 4}
 	sort.Ints(ints)
-	fmt.Println("Ints:   ", ints)
+	fmt.Println("Ints:", ints)
 
 	s := sort.IntsAreSorted(ints)
-	fmt.Println("sorted: ", s)
+	fmt.Println("Sorted:", s)
 }
