d693a234c0ab0e83a32d78d44392525ff33211e6
Justin Page <justin.page@motortrend.com>
2019-09-24 13:10:58 -0700

    Updated time example

--- 50.time.go
+++ 50.time.go
@@ -11,13 +11,13 @@ func main() {
 	now := time.Now()
 	p(now)
 
-	then := time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+	then := time.Date(
+		2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
 	p(then)
 
 	p(then.Year())
 	p(then.Month())
 	p(then.Day())
-	p(then.Hour())
 	p(then.Minute())
 	p(then.Second())
 	p(then.Nanosecond())
@@ -39,5 +39,4 @@ func main() {
 
 	p(then.Add(diff))
 	p(then.Add(-diff))
-
 }
